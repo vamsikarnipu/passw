7373b65b15bd1d115f35a66b72ee17ac598b9c52
vamsikarnipu <68433669+vamsikarnipu@users.noreply.github.com>
2023-07-26 16:18:04 +0530

    Add files via upload

---
+++ pass.js
@@ -0,0 +1,33 @@
+const passwordBox = document.querySelector('#password');
+const button = document.querySelector('button');
+const pass=document.querySelector(".pass")
+const length = 12;
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const numbers = '0123456789';
+const symbols = "!@#$%^&*()-=_+[]{}|;:'\"<>,.?/\\";
+const allChars = upperCase + lowerCase + numbers + symbols;
+
+function createPassword() {
+    let password = "";
+
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += numbers[Math.floor(Math.random() * numbers.length)];
+    password += symbols[Math.floor(Math.random() * symbols.length)];
+
+    while (length > password.length) {
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+
+    passwordBox.value = password;
+}
+function copyPassword()
+{
+    passwordBox.select();
+    document.execCommand('copy')
+}
+
+button.addEventListener('click', createPassword);
+
+pass.addEventListener('click', copyPassword);
